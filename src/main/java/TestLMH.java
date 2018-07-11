4b319048684b97d893ff4ae469924a0471c87596
lmh <876517344@qq.com>
2018-07-11 17:14:11 +0800

    测试分之冲突----master

--- src/main/java/TestLMH.java
+++ src/main/java/TestLMH.java
@@ -2,6 +2,6 @@ public class TestLMH {
     public static void main(String[] args) {
         System.out.println("测试分支");
         System.out.println("测试分支2");
-        System.out.println("测试分之冲突-----master");
+        System.out.println("测试分之冲突-----master l");
     }
 }
